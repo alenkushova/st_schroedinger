cbcbc51e3b5fa421901bcab44baca25a1c50d285
Alen <alen.kushova01@universitadipavia.it>
2022-03-10 01:40:20 +0100

    Changed initialize

--- initialize.m
+++ initialize.m
@@ -3,16 +3,12 @@ clear;
 
 currentpath = pwd;
 
-addpath (genpath ('geopdes-periodic'))
 addpath (genpath ('geopdes'))
 addpath (genpath ('nurbs'))
 addpath (genpath ('geopdes_hierarchical'))
 addpath (genpath ('data'))
-addpath (genpath ('quasi_interpolants'))
 addpath (genpath ('solvers'))
-addpath (genpath ('New_qi'))
 
-ex_laplace_iso_ring
-close all
+close 
 clear
 clc
